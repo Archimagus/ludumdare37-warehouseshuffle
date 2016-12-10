df11b33077c5c44d3e457844530a46ee5e8e72c7
David Casteel <davidwcasteel@gmail.com>
2016-12-10 02:38:08 -0600

    *Added truck base code * Added truck parking animation and code functionality (Hit Spacebar)

--- Warehouse/Assets/Scripts/GameManager.cs
+++ Warehouse/Assets/Scripts/GameManager.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] DeliveryTruckSlots;
+    public GameObject[] PickupTruckSlots;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Truck t;
+
+            if (Random.Range(0, 100) > 50)
+                t = DeliveryTruckSlots[Random.Range(0, DeliveryTruckSlots.Length)].GetComponentInChildren<Truck>();              
+            else
+                t = PickupTruckSlots[Random.Range(0, PickupTruckSlots.Length)].GetComponentInChildren<Truck>();
+
+            t = DeliveryTruckSlots[0].GetComponentInChildren<Truck>();
+
+            if (t.IsOffscreen)
+                t.Arrive();
+        }
+    }
 }
---
+++ Warehouse/Assets/Scripts/Truck.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Timers;
+using UnityEngine.UI;
+
+public class Truck : MonoBehaviour
+{
+    [SerializeField]
+    bool _isDeliveryTruck;
+    [SerializeField]
+    bool _randomTruckSize;
+    [SerializeField][Range(1, 3)]
+    int _truckSize;
+    [SerializeField]
+    Image _truckImage;
+
+    enum TruckState
+    {
+        OffScreen,
+        Arriving,
+        Waiting,
+        Leaving
+    }
+
+    TruckState _currState;
+    Animator _slotAnimator;
+
+    float _waitTime;
+    float _startTime;
+
+    int _deliveryValue;
+
+    public bool IsOffscreen { get { return _currState == TruckState.OffScreen; } }
+
+    // Use this for initialization
+    void Start()
+    {
+        _slotAnimator = GetComponentInParent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (_currState)
+        {
+            case TruckState.Arriving:
+                Arriving();
+                break;
+            case TruckState.Waiting:
+                Waiting();
+                break;
+            case TruckState.Leaving:
+                Leaving();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void Init()
+    {
+        if (_randomTruckSize)
+            _truckSize = Random.Range(1, 4);
+
+        _waitTime = _truckSize * 3.0f;
+        _deliveryValue = 0;
+    }
+
+    public void Arrive()
+    {
+        Init();
+        _currState = TruckState.Arriving;
+        _slotAnimator.SetTrigger("Park");
+    }
+
+    public void Leave()
+    {
+        _currState = TruckState.Leaving;
+        _slotAnimator.SetTrigger("Leave");
+    }
+
+    void Arriving()
+    {
+        if (_slotAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !_slotAnimator.IsInTransition(0))
+        {
+            _currState = TruckState.Waiting;
+        }
+    }
+
+    void Waiting()
+    {
+        _waitTime -= Time.deltaTime;
+
+        if (_waitTime <= 0)
+        {
+            ProcessPayment();
+
+            _currState = TruckState.Leaving;
+            Leave();
+        }
+    }
+
+    // Pays the player or penalizes them accordingly
+    void ProcessPayment()
+    {
+
+    }
+
+    void Leaving()
+    {
+        if (_slotAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !_slotAnimator.IsInTransition(0))
+        {
+            _currState = TruckState.OffScreen;
+        }
+    }
+}
