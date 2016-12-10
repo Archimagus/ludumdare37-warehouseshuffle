b7174785c26fe2b8f491eb7cee7157373e1bf9f4
Archimagus <excalibr8@yahoo.com>
2016-12-10 15:05:55 -0500

    Added readonly attribute

--- Warehouse/Assets/Scripts/GameManager.cs
+++ Warehouse/Assets/Scripts/GameManager.cs
@@ -1,34 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] DeliveryTruckSlots;
-    public GameObject[] PickupTruckSlots;
+	public GameObject[] DeliveryTruckSlots;
+	public GameObject[] PickupTruckSlots;
 
-    // Use this for initialization
-    void Start ()
-    {
-		
+	// Use this for initialization
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update ()
-    {
+	void Update()
+	{
 		if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Truck t;
+		{
+			Truck t;
 
-            if (Random.Range(0, 100) > 50)
-                t = DeliveryTruckSlots[Random.Range(0, DeliveryTruckSlots.Length)].GetComponentInChildren<Truck>();              
-            else
-                t = PickupTruckSlots[Random.Range(0, PickupTruckSlots.Length)].GetComponentInChildren<Truck>();
+			if (Random.Range(0, 100) > 50)
+				t = DeliveryTruckSlots[Random.Range(0, DeliveryTruckSlots.Length)].GetComponentInChildren<Truck>();
+			else
+				t = PickupTruckSlots[Random.Range(0, PickupTruckSlots.Length)].GetComponentInChildren<Truck>();
 
-            t = DeliveryTruckSlots[0].GetComponentInChildren<Truck>();
+			t = DeliveryTruckSlots[0].GetComponentInChildren<Truck>();
 
-            if (t.IsOffscreen)
-                t.Arrive();
-        }
-    }
+			if (t.IsOffscreen)
+				t.Arrive();
+		}
+	}
+}
+
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
 }
