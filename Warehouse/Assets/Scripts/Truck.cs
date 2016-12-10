9e5b7ba9972b6f01452a026868c55af1cee13dff
Archimagus <excalibr8@yahoo.com>
2016-12-10 05:08:46 -0500

    Fixed up the game object some to scale better with the screen.

--- Warehouse/Assets/Scripts/Truck.cs
+++ Warehouse/Assets/Scripts/Truck.cs
@@ -1,117 +1,117 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.Timers;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Truck : MonoBehaviour
 {
-    [SerializeField]
-    bool _isDeliveryTruck;
-    [SerializeField]
-    bool _randomTruckSize;
-    [SerializeField][Range(1, 3)]
-    int _truckSize;
-    [SerializeField]
-    Image _truckImage;
+	[SerializeField]
+	bool _isDeliveryTruck;
+	[SerializeField]
+	bool _randomTruckSize;
+	[SerializeField]
+	[Range(1, 3)]
+	int _truckSize;
+	[SerializeField]
+	Image _truckImage;
 
-    enum TruckState
-    {
-        OffScreen,
-        Arriving,
-        Waiting,
-        Leaving
-    }
+	enum TruckState
+	{
+		OffScreen,
+		Arriving,
+		Waiting,
+		Leaving
+	}
 
-    TruckState _currState;
-    Animator _slotAnimator;
+	TruckState _currState;
+	Animator _slotAnimator;
 
-    float _waitTime;
-    float _startTime;
+	float _waitTime;
+	float _startTime;
 
-    int _deliveryValue;
+	int _deliveryValue;
 
-    public bool IsOffscreen { get { return _currState == TruckState.OffScreen; } }
+	public bool IsOffscreen { get { return _currState == TruckState.OffScreen; } }
 
-    // Use this for initialization
-    void Start()
-    {
-        _slotAnimator = GetComponentInParent<Animator>();
-    }
+	// Use this for initialization
+	void Start()
+	{
+		_slotAnimator = GetComponentInParent<Animator>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        switch (_currState)
-        {
-            case TruckState.Arriving:
-                Arriving();
-                break;
-            case TruckState.Waiting:
-                Waiting();
-                break;
-            case TruckState.Leaving:
-                Leaving();
-                break;
-            default:
-                break;
-        }
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		switch (_currState)
+		{
+			case TruckState.Arriving:
+				Arriving();
+				break;
+			case TruckState.Waiting:
+				Waiting();
+				break;
+			case TruckState.Leaving:
+				Leaving();
+				break;
+			default:
+				break;
+		}
+	}
 
-    private void Init()
-    {
-        if (_randomTruckSize)
-            _truckSize = Random.Range(1, 4);
+	private void Init()
+	{
+		if (_randomTruckSize)
+			_truckSize = Random.Range(1, 4);
 
-        _waitTime = _truckSize * 3.0f;
-        _deliveryValue = 0;
-    }
+		_waitTime = _truckSize * 3.0f;
+		_deliveryValue = 0;
+	}
 
-    public void Arrive()
-    {
-        Init();
-        _currState = TruckState.Arriving;
-        _slotAnimator.SetTrigger("Park");
-    }
+	public void Arrive()
+	{
+		Init();
+		_currState = TruckState.Arriving;
+		_slotAnimator.SetTrigger("Park");
+		GetComponent<AudioSource>().Play();
+	}
 
-    public void Leave()
-    {
-        _currState = TruckState.Leaving;
-        _slotAnimator.SetTrigger("Leave");
-    }
+	public void Leave()
+	{
+		_currState = TruckState.Leaving;
+		_slotAnimator.SetTrigger("Leave");
+	}
 
-    void Arriving()
-    {
-        if (_slotAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !_slotAnimator.IsInTransition(0))
-        {
-            _currState = TruckState.Waiting;
-        }
-    }
+	void Arriving()
+	{
+		if (_slotAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !_slotAnimator.IsInTransition(0))
+		{
+			_currState = TruckState.Waiting;
+			GetComponent<AudioSource>().Stop();
+		}
+	}
 
-    void Waiting()
-    {
-        _waitTime -= Time.deltaTime;
+	void Waiting()
+	{
+		_waitTime -= Time.deltaTime;
 
-        if (_waitTime <= 0)
-        {
-            ProcessPayment();
+		if (_waitTime <= 0)
+		{
+			ProcessPayment();
 
-            _currState = TruckState.Leaving;
-            Leave();
-        }
-    }
+			_currState = TruckState.Leaving;
+			Leave();
+		}
+	}
 
-    // Pays the player or penalizes them accordingly
-    void ProcessPayment()
-    {
+	// Pays the player or penalizes them accordingly
+	void ProcessPayment()
+	{
 
-    }
+	}
 
-    void Leaving()
-    {
-        if (_slotAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !_slotAnimator.IsInTransition(0))
-        {
-            _currState = TruckState.OffScreen;
-        }
-    }
+	void Leaving()
+	{
+		if (_slotAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !_slotAnimator.IsInTransition(0))
+		{
+			_currState = TruckState.OffScreen;
+		}
+	}
 }
--- Warehouse/Assets/Scripts/Warehouse.cs
+++ Warehouse/Assets/Scripts/Warehouse.cs
@@ -1,16 +1,86 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Warehouse : MonoBehaviour {
+public class Warehouse : MonoBehaviour
+{
+	private const int MaxGridSize = 5;
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField]
+	private RectTransform _frameRect;
+
+	[SerializeField]
+	[Range(1, MaxGridSize)]
+	private int _startingRows = 2;
+	[SerializeField]
+	[Range(1, MaxGridSize)]
+	private int _startingCols = 2;
+	[SerializeField]
+	private int _gridElementSize = 64;
+	[SerializeField]
+	private int _gridSpacing = 1;
+	[SerializeField]
+	private int _borderSize = 80;
+	private int _rows;
+	private int _cols;
+
+
+	enum Space
+	{
+		Unavailable,
+		Occupied,
+		Available
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private Space[,] _spaces = new Space[MaxGridSize * 2, MaxGridSize * 2];
+
+
+	void Start()
+	{
+		_rows = _startingRows;
+		_cols = _startingCols;
+		ResizeGrid();
+	}
+
+	void OnValidate()
+	{
+		_rows = _startingRows;
+		_cols = _startingCols;
+		ResizeGrid();
+	}
+
+	public void ExpandRight()
+	{
+		var offset = _frameRect.offsetMax;
+		offset.x += _gridElementSize + _gridSpacing;
+		_frameRect.offsetMax = offset;
+	}
+	public void ExpandLeft()
+	{
+		var offset = _frameRect.offsetMin;
+		offset.x -= _gridElementSize + _gridSpacing;
+		_frameRect.offsetMin = offset;
+	}
+	public void ExpandUp()
+	{
+		var offset = _frameRect.offsetMax;
+		offset.y += _gridElementSize + _gridSpacing;
+		_frameRect.offsetMax = offset;
+	}
+	public void ExpandDown()
+	{
+		var offset = _frameRect.offsetMin;
+		offset.y -= _gridElementSize + _gridSpacing;
+		_frameRect.offsetMin = offset;
+	}
+	public void ResizeGrid()
+	{
+		var offset = _frameRect.offsetMax;
+		offset.x = (_gridElementSize + _gridSpacing) * (_rows) + _borderSize;
+		offset.y = (_gridElementSize + _gridSpacing) * (_cols) + _borderSize;
+		_frameRect.offsetMax = offset;
+
+		offset = _frameRect.offsetMin;
+		offset.x = -((_gridElementSize + _gridSpacing) * (_rows) + _borderSize);
+		offset.y = -((_gridElementSize + _gridSpacing) * (_cols) + _borderSize);
+		_frameRect.offsetMin = offset;
 	}
 }
