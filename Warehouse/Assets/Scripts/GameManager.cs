66c4dd73f2ef96e080bbf76a3c2357fc78291a1b
Archimagus <excalibr8@yahoo.com>
2016-12-10 15:27:10 -0500

    no message

--- Warehouse/Assets/Scripts/GameManager.cs
+++ Warehouse/Assets/Scripts/GameManager.cs
@@ -1,94 +1,92 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] DeliveryTruckSlots;
-    public GameObject[] PickupTruckSlots;
+	public GameObject[] DeliveryTruckSlots;
+	public GameObject[] PickupTruckSlots;
 
-    [SerializeField]
-    float _deliveryTruckSpawnDelay;
-    [SerializeField]
-    float _pickupTruckSpawnDelay;
-    [SerializeField]
-    int _maxDeliveryTrucks;
-    [SerializeField]
-    int _maxPickupTrucks;
-    [SerializeField]
-    float _deliveryTruckSpawnTimer;
-    [SerializeField]
-    float _pickupTruckSpawnTimer;
-    [SerializeField]
-    int _currDeliveryTrucks;
-    [SerializeField]
-    int _currPickupTrucks;
+	[SerializeField]
+	float _deliveryTruckSpawnDelay;
+	[SerializeField]
+	float _pickupTruckSpawnDelay;
+	[SerializeField]
+	int _maxDeliveryTrucks;
+	[SerializeField]
+	int _maxPickupTrucks;
+	[SerializeField]
+	float _deliveryTruckSpawnTimer;
+	[SerializeField]
+	float _pickupTruckSpawnTimer;
+	[SerializeField]
+	int _currDeliveryTrucks;
+	[SerializeField]
+	int _currPickupTrucks;
 
-    private static GameManager _instance;
-    public static GameManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-                _instance = FindObjectOfType<GameManager>();
-            if (_instance == null)
-            {
-                Debug.LogError("No GameManager in the scene");
-            }
-            return _instance;
-        }
-    }
-
-    // Use this for initialization
-    void Start ()
-    {
-        Init();
+	private static GameManager _instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			if (_instance == null)
+				_instance = FindObjectOfType<GameManager>();
+			if (_instance == null)
+			{
+				Debug.LogError("No GameManager in the scene");
+			}
+			return _instance;
+		}
 	}
-	
+
+	// Use this for initialization
+	void Start()
+	{
+		Init();
+	}
+
 	// Update is called once per frame
-	void Update ()
-    {
-        SpawnTruck();
-    }
+	void Update()
+	{
+		SpawnTruck();
+	}
 
-    void Init()
-    {
-        _deliveryTruckSpawnTimer = _deliveryTruckSpawnDelay = 6;
-        _pickupTruckSpawnTimer = _pickupTruckSpawnDelay = 6;
-        _maxDeliveryTrucks = 3;
-        _maxPickupTrucks = 3;
-        _currDeliveryTrucks = 0;
-        _currPickupTrucks = 0;
-    }
+	void Init()
+	{
+		_deliveryTruckSpawnTimer = _deliveryTruckSpawnDelay = 6;
+		_pickupTruckSpawnTimer = _pickupTruckSpawnDelay = 6;
+		_maxDeliveryTrucks = 3;
+		_maxPickupTrucks = 3;
+		_currDeliveryTrucks = 0;
+		_currPickupTrucks = 0;
+	}
 
-    public void TruckIsOffscreen(bool isDeliveryTruck)
-    {
-        if (isDeliveryTruck)
-            _currDeliveryTrucks--;
-        else
-            _currPickupTrucks--;
-    }
+	public void TruckIsOffscreen(bool isDeliveryTruck)
+	{
+		if (isDeliveryTruck)
+			_currDeliveryTrucks--;
+		else
+			_currPickupTrucks--;
+	}
 
-    void SpawnTruck()
-    {
-        _deliveryTruckSpawnTimer -= Time.deltaTime;
+	void SpawnTruck()
+	{
+		_deliveryTruckSpawnTimer -= Time.deltaTime;
 
-        if (_deliveryTruckSpawnTimer <= 0.0f)
-        {
-            _deliveryTruckSpawnTimer = _deliveryTruckSpawnDelay;
+		if (_deliveryTruckSpawnTimer <= 0.0f)
+		{
+			_deliveryTruckSpawnTimer = _deliveryTruckSpawnDelay;
 
-            if (_currDeliveryTrucks < _maxDeliveryTrucks)
-            {
-                Truck t = DeliveryTruckSlots[Random.Range(0, DeliveryTruckSlots.Length)].GetComponentInChildren<Truck>();
+			if (_currDeliveryTrucks < _maxDeliveryTrucks)
+			{
+				Truck t = DeliveryTruckSlots[Random.Range(0, DeliveryTruckSlots.Length)].GetComponentInChildren<Truck>();
 
-                while (!t.IsOffscreen)
-                {
-                    t = DeliveryTruckSlots[Random.Range(0, DeliveryTruckSlots.Length)].GetComponentInChildren<Truck>();
-                }
+				while (!t.IsOffscreen)
+				{
+					t = DeliveryTruckSlots[Random.Range(0, DeliveryTruckSlots.Length)].GetComponentInChildren<Truck>();
+				}
 
-                t.Arrive();
-                _currDeliveryTrucks++;
-            }
-        }
-    }
+				t.Arrive();
+				_currDeliveryTrucks++;
+			}
+		}
+	}
 }
