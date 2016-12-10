65bf372fcc52aba2f2bc9b586b0a1790d2a243b2
Archimagus <excalibr8@yahoo.com>
2016-12-10 13:08:31 -0500

    Grid Work

---
+++ Warehouse/Assets/Scripts/DynaGrid.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public enum ExpandDir
+{
+	Left, Up, Right, Down
+}
+public class DynaGrid<T>
+{
+	public int Rows { get; private set; }
+	public int Cols { get; private set; }
+
+	private T[,] _grid;
+
+	public DynaGrid(int rows, int cols)
+	{
+		Rows = rows;
+		Cols = cols;
+
+		_grid = new T[rows, cols];
+	}
+
+	public T this[int r, int c]
+	{
+		get { return _grid[r, c]; }
+		set { _grid[r, c] = value; }
+	}
+
+	public void Expand(ExpandDir dir)
+	{
+		switch (dir)
+		{
+			case ExpandDir.Left:
+				{
+					Cols++;
+					var g = new T[Rows, Cols];
+					for (int r = 0; r < Rows; r++)
+					{
+						for (int c = 1; c < Cols; c++)
+						{
+							g[r, c] = _grid[r, c - 1];
+						}
+					}
+					_grid = g;
+				}
+				break;
+			case ExpandDir.Right:
+				{
+					Cols++;
+					var g = new T[Rows, Cols];
+					for (int r = 0; r < Rows; r++)
+					{
+						for (int c = 0; c < Cols - 1; c++)
+						{
+							g[r, c] = _grid[r, c];
+						}
+					}
+					_grid = g;
+				}
+				break;
+			case ExpandDir.Up:
+				{
+					Rows++;
+					var g = new T[Rows, Cols];
+					for (int r = 1; r < Rows; r++)
+					{
+						for (int c = 0; c < Cols; c++)
+						{
+							g[r, c] = _grid[r - 1, c];
+						}
+					}
+					_grid = g;
+				}
+				break;
+			case ExpandDir.Down:
+				{
+					Rows++;
+					var g = new T[Rows, Cols];
+					for (int r = 0; r < Rows - 1; r++)
+					{
+						for (int c = 0; c < Cols; c++)
+						{
+							g[r, c] = _grid[r, c];
+						}
+					}
+					_grid = g;
+				}
+				break;
+			default:
+				throw new ArgumentOutOfRangeException("dir", dir, null);
+		}
+	}
+}
--- Warehouse/Assets/Scripts/Warehouse.cs
+++ Warehouse/Assets/Scripts/Warehouse.cs
@@ -1,11 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Warehouse : MonoBehaviour
+enum Space
+{
+	Unavailable,
+	Occupied,
+	Available
+}
+public class Warehouse : MonoBehaviour, IDragHandler
 {
 	private const int MaxGridSize = 5;
 
 	[SerializeField]
 	private RectTransform _frameRect;
+	[SerializeField]
+	private Image _floorImage;
+
+	[Header("Prefabs")]
+	[SerializeField]
+	private Image _tilePrefab;
 
 	[SerializeField]
 	[Range(1, MaxGridSize)]
@@ -19,68 +34,99 @@ public class Warehouse : MonoBehaviour
 	private int _gridSpacing = 1;
 	[SerializeField]
 	private int _borderSize = 80;
-	private int _rows;
-	private int _cols;
-
-
-	enum Space
-	{
-		Unavailable,
-		Occupied,
-		Available
-	}
-
-	private Space[,] _spaces = new Space[MaxGridSize * 2, MaxGridSize * 2];
+	DynaGrid<Space> _spaces;
 
 
 	void Start()
 	{
-		_rows = _startingRows;
-		_cols = _startingCols;
 		ResizeGrid();
+		RepopulateGrid();
 	}
 
 	void OnValidate()
 	{
-		_rows = _startingRows;
-		_cols = _startingCols;
 		ResizeGrid();
 	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		Debug.Log(new string(eventData.hovered.SelectMany(h => h.name + " ").ToArray()));
+	}
+
+	//void Update()
+	//{
+	//	var mouse = Input.mousePosition;
+	//	mouse = _floorImage.rectTransform.InverseTransformPoint(mouse);
+	//	mouse.x += _floorImage.rectTransform.rect.width / 2;
+	//	mouse.y += _floorImage.rectTransform.rect.height / 2;
+	//	Debug.Log(mouse);
+	//}
 
 	public void ExpandRight()
 	{
 		var offset = _frameRect.offsetMax;
 		offset.x += _gridElementSize + _gridSpacing;
 		_frameRect.offsetMax = offset;
+		_spaces.Expand(ExpandDir.Right);
+		RepopulateGrid();
 	}
 	public void ExpandLeft()
 	{
 		var offset = _frameRect.offsetMin;
 		offset.x -= _gridElementSize + _gridSpacing;
 		_frameRect.offsetMin = offset;
+		_spaces.Expand(ExpandDir.Left);
+		RepopulateGrid();
 	}
 	public void ExpandUp()
 	{
 		var offset = _frameRect.offsetMax;
 		offset.y += _gridElementSize + _gridSpacing;
 		_frameRect.offsetMax = offset;
+		_spaces.Expand(ExpandDir.Up);
+		RepopulateGrid();
 	}
 	public void ExpandDown()
 	{
 		var offset = _frameRect.offsetMin;
 		offset.y -= _gridElementSize + _gridSpacing;
 		_frameRect.offsetMin = offset;
+		_spaces.Expand(ExpandDir.Down);
+		RepopulateGrid();
+	}
+	public void RepopulateGrid()
+	{
+		while (_floorImage.transform.childCount > 0)
+		{
+			var c = _floorImage.transform.GetChild(0);
+			c.SetParent(null);
+			Destroy(c.gameObject);
+		}
+
+		for (int i = 0; i < _spaces.Rows; i++)
+		{
+			for (int j = 0; j < _spaces.Cols; j++)
+			{
+				var c = Instantiate(_tilePrefab);
+				c.color = Color.green;
+				c.transform.SetParent(_floorImage.transform);
+			}
+		}
+
 	}
 	public void ResizeGrid()
 	{
+		_spaces = new DynaGrid<Space>(_startingRows, _startingCols);
+
 		var offset = _frameRect.offsetMax;
-		offset.x = (_gridElementSize + _gridSpacing) * (_rows) + _borderSize;
-		offset.y = (_gridElementSize + _gridSpacing) * (_cols) + _borderSize;
+		offset.x = (_gridElementSize + _gridSpacing) * (_startingCols / 2f) + _borderSize;
+		offset.y = (_gridElementSize + _gridSpacing) * (_startingRows / 2f) + _borderSize;
 		_frameRect.offsetMax = offset;
 
 		offset = _frameRect.offsetMin;
-		offset.x = -((_gridElementSize + _gridSpacing) * (_rows) + _borderSize);
-		offset.y = -((_gridElementSize + _gridSpacing) * (_cols) + _borderSize);
+		offset.x = -((_gridElementSize + _gridSpacing) * (_startingCols / 2f) + _borderSize);
+		offset.y = -((_gridElementSize + _gridSpacing) * (_startingRows / 2f) + _borderSize);
 		_frameRect.offsetMin = offset;
 	}
+
 }
